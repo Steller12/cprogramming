822d1092af9527b3f120c6ff46593ec0f606c28a
Pushp <pugpta2601@gmail.com>
2023-02-08 16:45:28 +0530

    c program

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -4,18 +4,41 @@ int main(){
 //	int d=++a;
 //	int b=5;
 //	char c=(char)b;
-//	&& id AND operator || is OR gate and ! is NOT gat
+//	&& is AND operator || is OR gate and ! is NOT gat
 //++a first increment then use the value and a++ id first use the original value then increment same with --a and a--
+//10<<2 is left shift and 10>>2 is right shift
 //	printf("%d\n",sizeof(a));
 //	printf("%d\n",c);
 //	printf("%d\n%d",d,a);
-	int a,b;
-	scanf("%d",&a);
-	scanf("%d",&b);
-	a=a+b;
-	b=a-b;
-	a=a-b;
-	printf("%d\n",a);
-	printf("%d",b);
+//	int a,b;
+//	scanf("%d",&a);
+//	scanf("%d",&b);
+//	a=a+b;
+//	b=a-b;
+//	a=a-b;
+//	printf("%d\n",a);
+//	printf("%d",b);
+//	int a=9;
+//	int c=--a - a--;
+//	printf("%d %d",c,a);
+//
+//	scanf("%s",&a);
+//	printf("Hello world!\n%s",a)
+//	int a=(1,2,3,4,5);
+//	double b;
+//	printf("%d",a);
+//	printf("%d,%d\n",sizeof(a),sizeof(b));
+//	float pi=3.14;
+//	int c=(int)pi;
+//	printf("%d",c);
+	char ch;
+	char s[100];
+	char sen[100];
+	scanf("%c",ch);
+	scanf("\n");
+	scanf("%s",s);
+	scanf("\n");
+	gets(sen);
+	printf("%c\n%s\n%s",ch,s,sen);
 	return 0;
 }
